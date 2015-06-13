a327d10c5fd2f92a32c774a61c5e9becedca1449
Bill Dengler <codeofdusk@gmail.com>
2015-06-13 14:23:41 -0400

    Fixes.

--- src/application.py
+++ src/application.py
@@ -10,5 +10,5 @@ description = unicode(name+" is a fork of TWBlue, an app designed to use Twitter
 translators = [u"Bryner Villalobos, Bill Dengler (English)", u"Mohammed Al Shara (Arabic)", u"Joan Rabat, Juan Carlos Rivilla (Catalan)", u"Manuel cortéz (Spanish)", u"Sukil Etxenike Arizaleta (Basque)", u"Jani Kinnunen (finnish)", u"Rémy Ruiz (French)", u"Alba Quinteiro (Galician)", u"Steffen Schultz (German)", u"Robert Osztolykan (Hungarian)", u"Paweł Masarczyk (Polish)", u"Odenilton Júnior Santos (Portuguese)", u"Alexander Jaszyn (Russian)", u"Burak (Turkish)"]
 url = u"http://n6.io/magenta"
 report_bugs_url = "http://twblue.es/bugs/api/soap/mantisconnect.php?wsdl"
-API_key_overide="drOUqAy1LG7SpL7dv4Vj0zqwd"
-API_secret_overide="v96WDb9x5mcrdk0SOcAXmB9V84aqY7nn33yFev74qOFcvbe8XH"
+api_key_override="drOUqAy1LG7SpL7dv4Vj0zqwd"
+api_secret_override="v96WDb9x5mcrdk0SOcAXmB9V84aqY7nn33yFev74qOFcvbe8XH"
