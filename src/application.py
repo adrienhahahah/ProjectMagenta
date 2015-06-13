405e501f56849050b6095ca23f0d245c8700d32f
Bill Dengler <codeofdusk@gmail.com>
2015-06-13 14:02:47 -0400

    Fixed indentation.

--- src/application.py
+++ src/application.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 name = 'TWMagenta'
 snapshot = True
- version = "0.0.0"
- update_url = 'http://n6.io/magenta/update.json'
+version = "0.0.0"
+update_url = 'http://n6.io/magenta/update.json'
 author = u"Bill Dengler"
 authorEmail = "codeofdusk@gmail.com"
 copyright = u"Copyright (C) 2015, Bill Dengler\nBased on TWBlue, Copyright (C) 2015, Technow S.L."
@@ -10,5 +10,5 @@ description = unicode(name+" is a fork of TWBlue, an app designed to use Twitter
 translators = [u"Bryner Villalobos, Bill Dengler (English)", u"Mohammed Al Shara (Arabic)", u"Joan Rabat, Juan Carlos Rivilla (Catalan)", u"Manuel cortéz (Spanish)", u"Sukil Etxenike Arizaleta (Basque)", u"Jani Kinnunen (finnish)", u"Rémy Ruiz (French)", u"Alba Quinteiro (Galician)", u"Steffen Schultz (German)", u"Robert Osztolykan (Hungarian)", u"Paweł Masarczyk (Polish)", u"Odenilton Júnior Santos (Portuguese)", u"Alexander Jaszyn (Russian)", u"Burak (Turkish)"]
 url = u"http://n6.io/magenta"
 report_bugs_url = "http://twblue.es/bugs/api/soap/mantisconnect.php?wsdl"
-api_key_overide="drOUqAy1LG7SpL7dv4Vj0zqwd"
-api_secret_overide="v96WDb9x5mcrdk0SOcAXmB9V84aqY7nn33yFev74qOFcvbe8XH"
+API_key_overide="drOUqAy1LG7SpL7dv4Vj0zqwd"
+API_secret_overide="v96WDb9x5mcrdk0SOcAXmB9V84aqY7nn33yFev74qOFcvbe8XH"
