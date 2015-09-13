0389848116c8413dc928a8c83e05f60c23813415
Bill Dengler <codeofdusk@gmail.com>
2015-09-13 15:02:42 -0400

    magentification.

--- src/main.py
+++ src/main.py
@@ -89,7 +89,7 @@ def setup():
 def donation():
  dlg = commonMessageDialogs.donation()
  if dlg == widgetUtils.YES:
-  webbrowser.open_new_tab("http://twblue.es/?q=donate")
+  webbrowser.open_new_tab("http://n6.io/magenta/donate.htm")
  config.app["app-settings"]["donation_dialog_displayed"] = True
 
 setup()
