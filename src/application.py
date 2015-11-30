84210fc5d20101097d06bea957ba57ba6e0aba06
Bill Dengler <codeofdusk@gmail.com>
2015-11-29 19:04:49 -0500

    Prepare application.py for transition mode.

--- src/application.py
+++ src/application.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-name = 'Project Magenta'
+name = 'Transition Mode'
 snapshot=True
-version = "4"
-update_url = 'http://n6.io/magenta/builds/update.json'
+version = "0"
+update_url = 'http://twblue.es/updates/snapshots_ngen.json'
 author = u"Bill Dengler"
 authorEmail = "codeofdusk@gmail.com"
-copyright = u"Copyright (C) 2015, Bill Dengler\nBased on TWBlue, Copyright (C) 2015, Technow S.L."
-description = unicode(name+" is a fork of TWBlue, an app designed to use Twitter simply and efficiently while using minimal system resources. This fork enhanses the original project and pulls in changes from it frequently.")
+copyright = u"Copyright (C) 2015, Bill Dengler."
+description = unicode(name+" is a hacked-together solution to facilitate the switch of Project Magenta users to TWBlue's snapshot repo (but you really should never be seeing this).")
 translators = [u"Bryner Villalobos, Bill Dengler (English)", u"Mohammed Al Shara (Arabic)", u"Joan Rabat, Juan Carlos Rivilla (Catalan)", u"Manuel cortéz (Spanish)", u"Sukil Etxenike Arizaleta (Basque)", u"Jani Kinnunen (finnish)", u"Rémy Ruiz (French)", u"Alba Quinteiro (Galician)", u"Steffen Schultz (German)", u"Robert Osztolykan (Hungarian)", u"Paweł Masarczyk (Polish)", u"Odenilton Júnior Santos (Portuguese)", u"Alexander Jaszyn (Russian)", u"Burak (Turkish)"]
 url = u"http://n6.io/magenta"
 report_bugs_url = "http://twblue.es/bugs/api/soap/mantisconnect.php?wsdl"
-api_key_override="drOUqAy1LG7SpL7dv4Vj0zqwd"
-api_secret_override="v96WDb9x5mcrdk0SOcAXmB9V84aqY7nn33yFev74qOFcvbe8XH"
+api_key_override="You are a large metallic dingus."
+api_secret_override="Eres un dingus grande hecho de metal."
